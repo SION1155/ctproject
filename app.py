b665803ed3862423e33339acd3bafd78dcec3b7f
SION1155 <seizetheday@g.cnees.kr>
2025-07-30 13:58:39 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
 import streamlit.components.v1 as htmlviewer
-st.set_page_config(layout='wide', page_title='DNA sequencying!!!')
+st.set_page_config(layout='wide', page_title='hanmoon')
 # Title Msg#1
 st.title('This is sion Webapp!!')
 
-with open('./index.html', 'r', encoding='utf-8') as f:
-    html = f.read()
+with open('./index1.html', 'r', encoding='utf-8') as f:
+    html1 = f.read()
+    f.close()
+with open('./index2.html', 'r', encoding='utf-8') as f:
+    html2 = f.read()
+    f.close()
+with open('./index3.html', 'r', encoding='utf-8') as f:
+    html3 = f.read()
     f.close()
 
 #html = '''
@@ -23,19 +29,22 @@ with open('./index.html', 'r', encoding='utf-8') as f:
 # Box#1(4), Box#2(1)
 col1, col2 = st.columns((4,1))
 with col1:
-    with st.expander('Content #1...'):
+    with st.expander('Content #video...'):
         url = 'https://www.youtube.com/watch?v=XyEOEBsa8I4'
         st.info('Content..')
         st.video(url)
     with st.expander('image content...'):
         imgfilepath = './images/han.jpg'
         st.image(imgfilepath)
-    with st.expander('Content #2...'):
+    with st.expander('Content #CT1...'):
         #st.write(html, unsafe_allow_html=True)
-        htmlviewer.html(html, height=600)
-    with st.expander('Content #3...'):
+        htmlviewer.html(html1, height=1000)
+    with st.expander('Content #CT2...'):
         #st.write(html, unsafe_allow_html=True)
-        htmlviewer.html(html, height=600)
+        htmlviewer.html(html2, height=1000)
+    with st.expander('Content #AIX...'):
+        #st.write(html, unsafe_allow_html=True)
+        htmlviewer.html(html3, height=2500)
 with col2:
     with st.expander('Tips..'):
         st.info('Tips..')
